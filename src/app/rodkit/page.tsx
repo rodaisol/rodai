bd998dd0a9e6547c2d3711e5351c0779f668774d
llegoelkelo <kelvin@cryptonians.xyz>
2024-03-21 13:17:35 -0400

    update page metadata for rodkit roadmap page

--- src/app/rodkit/page.tsx
+++ src/app/rodkit/page.tsx
@@ -1,4 +1,12 @@
+import { Metadata } from 'next'
+
 import { RodKitRoadmapPage } from '../../pageComponents/RodKitRoadmap'
+
+export const metadata: Metadata = {
+  title: 'RODAI - Introducing RODKit',
+  description:
+    'The RODKit, enabling anyone to launch community projects on Solana.',
+}
 
 export default function Home() {
   return <RodKitRoadmapPage />
