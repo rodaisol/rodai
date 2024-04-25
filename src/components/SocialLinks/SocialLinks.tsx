36eda2049710124b0626b1216b42631bb363e40f
llegoelkelo <kelvin@cryptonians.xyz>
2024-04-24 23:11:15 -0400

    update social links

--- src/components/SocialLinks/SocialLinks.tsx
+++ src/components/SocialLinks/SocialLinks.tsx
@@ -18,28 +18,42 @@ export const SocialLinks: FC<SocialLinksProps> = ({ className }) => {
         <img
           src="/img/social/telegram.png"
           alt="telegram"
-          className="w-12 md:w-16 cursor-pointer"
+          className="w-10 md:w-12 cursor-pointer"
         />
       </Link>
       <Link href="https://twitter.com/aerosol_ai" target="_blank">
         <img
           src="/img/social/x.png"
           alt="twitter"
-          className="w-12 md:w-16 cursor-pointer"
+          className="w-10 md:w-12 cursor-pointer"
         />
       </Link>
       <Link href="https://discord.gg/rodai" target="_blank">
         <img
           src="/img/social/discord.png"
           alt="discord"
-          className="w-12 md:w-16 cursor-pointer"
+          className="w-10 md:w-12 cursor-pointer"
         />
       </Link>
       <Link href="https://youtube.com/@RodAISol" target="_blank">
         <img
           src="/img/social/youtube.png"
           alt="youtube"
-          className="w-12 md:w-16 cursor-pointer"
+          className="w-10 md:w-12 cursor-pointer"
+        />
+      </Link>
+      <Link href="https://www.instagram.com/rodai.meme" target="_blank">
+        <img
+          src="/img/social/instagram.webp"
+          alt="instagram"
+          className="w-10 md:w-12 cursor-pointer rounded-full"
+        />
+      </Link>
+      <Link href="https://www.tiktok.com/@rodai.meme" target="_blank">
+        <img
+          src="/img/social/tiktok.png"
+          alt="tiktok"
+          className="w-10 md:w-12 cursor-pointer rounded-full"
         />
       </Link>
     </div>
